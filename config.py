2a0301648aa14fb44e07be6ccb37a8585b673e5c
Jaime Lopez <jaime.meriz13@gmail.com>
2021-11-11 17:52:46 -0800

    email notify protype working

--- config.py
+++ config.py
@@ -1,8 +1,10 @@
 from pathlib import Path
 
 # Read the app_id and app_key required for MathPix API.
-app_id_path = Path("configs/app_user.txt")
-app_key_path = Path("configs/app_password.txt")
+app_user_path = Path("configs/app_user.txt")
+app_password_path = Path("configs/app_password.txt")
+app_reviewer_path = Path("configs/app_reviewer.txt")
 
 APP_USER = app_user_path.read_text()
 APP_PASSWORD = app_password_path.read_text()
+APP_REVIEWER = app_reviewer_path.read_text()
